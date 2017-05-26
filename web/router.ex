e14d016cfbde2a3d156172d015b91975c2c1cb69
banzay <yuriy.zdyrko@gmail.com>
2017-05-26 12:25:35 +0300

    videos 1

--- web/router.ex
+++ web/router.ex
@@ -21,7 +21,13 @@ defmodule Rumbl.Router do
     # get "/users/:id", UserController, :show
     resources "/users", UserController, only: [:index, :show, :new, :create]
     resources "/sessions", SessionController, only: [:new, :create, :delete]
+
     get "/", PageController, :index
+  end
+
+  scope "/manage", Rumbl do
+    pipe_through [:browser, :authenticate_user]
+    resources "/videos", VideoController
   end
 
   # Other scopes may use custom stacks.
